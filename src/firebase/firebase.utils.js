3277ad5f2a99d51a475060c09cf21cc624e6c76c
Juan Esteban <juan.es-19@hotmail.com>
2020-02-24 07:43:17 -0500

    createUserProfileDocument function created to allow create users when no added to the database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,6 +18,31 @@ firebase.initializeApp(config);
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if(!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err) {
+      console.log('Error creating user: ', err.message);
+    }
+  }
+  
+  return userRef;
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const signInWithGoogle = () => auth.signInWithPopup(provider);

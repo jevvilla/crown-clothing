26c05552319b948abaaeb44e06f988802af0980a
Juan Esteban <juan.es-19@hotmail.com>
2020-02-24 07:46:55 -0500

    App.jsx updated, createUserProfileDocument function is implmented in this screen to register user on login action

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUpPage from "./pages/signin-signup/signin-signup.component";
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import Header from "./components/header/header.component";
 
@@ -14,14 +14,28 @@ class App extends React.PureComponent {
   constructor() {
     super();
 
+    this.unsubscribeFromAuth = null;
     this.state = {
       currentUser: null
     }
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 

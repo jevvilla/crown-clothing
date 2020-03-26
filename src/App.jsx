99e3fb9814fddfcbd28f5479a8bd5845f8b88782
Juan Esteban Villa <jvilla@MC02ZV1DCMD6M.local>
2020-03-26 15:37:14 -0500

    userReducer and userAction implemented to Header and App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUpPage from "./pages/signin-signup/signin-signup.component";
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from "./redux/user/user.actions";
 
 import Header from "./components/header/header.component";
 
 import "./App.css";
 
 class App extends React.PureComponent {
-  constructor() {
-    super();
-
-    this.unsubscribeFromAuth = null;
-    this.state = {
-      currentUser: null
-    }
-  }
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth) {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data
-            }
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
-      } else {
-        this.setState({ currentUser: userAuth });
       }
+
+      setCurrentUser(userAuth);
     });
   }
 
@@ -46,7 +41,7 @@ class App extends React.PureComponent {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -57,4 +52,8 @@ class App extends React.PureComponent {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);

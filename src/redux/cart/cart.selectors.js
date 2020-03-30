a6402531d816c84e5221776a2b7e7beb3f900435
jevvilla <juan.es-19@hotmail.com>
2020-03-30 18:12:50 -0500

    cartTotalSelector created to have a total amount of items in cart

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -10,3 +10,12 @@ export const cartItemsCountSelector = createSelector(
   [cartItemsSelector],
   cartItems => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
 );
+
+export const cartTotalSelector = createSelector(
+  [cartItemsSelector],
+  cartItems =>
+    cartItems.reduce(
+      (acc, cartItem) => acc + cartItem.quantity * cartItem.price,
+      0
+    )
+);

2b137b78c72765645967d6e0605e83e5a417f07a
jevvilla <juan.es-19@hotmail.com>
2020-04-05 17:33:32 -0500

    data normalization, shop data arrays were converted to objects to improve search process

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,9 @@ export const shopColectionsSelector = createSelector(
   [shopSelector],
   (shop) => shop.collections
 );
+
+export const collectionSelector = (collectionUrlParam) =>
+  createSelector(
+    [shopColectionsSelector],
+    (collections) => collections[collectionUrlParam]
+  );

61f98d8ac9460179609f84b7b8881f9900744096
Jevvilla <juan.es-19@hotmail.com>
2020-04-23 08:10:05 -0500

    Custom buttom component moved to styled component, sass styles were removed

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 
-import "./custom-button.styles.scss";
+import { CustomButtonComponent } from "./custom-button.styles";
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  <button
-    className={`${inverted ? "inverted" : ""} ${
-      isGoogleSignIn ? "google-sign-in" : ""
-    } custom-button`}
-    {...otherProps}
-  >
-    {children}
-  </button>
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonComponent {...props}>{children}</CustomButtonComponent>
 );
 
 export default CustomButton;

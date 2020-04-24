c12df54219117a4f001d6252e6b2ced742f4e632
Jevvilla <juan.es-19@hotmail.com>
2020-04-24 08:26:55 -0500

    SignInAndSignup screen was migrated to styled components

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import SignIn from "../../components/sign-in/sign-in.component";
 import SignUp from "../../components/sign-up/sign-up.component";
 
-import "./signin-signup.styles.scss";
+import { SignInAndSignUpContainer } from "./signin-signup.styles";
 
 const SignInSignUpPage = () => (
-  <div className="sign-in-and-sign-up">
+  <SignInAndSignUpContainer>
     <SignIn />
     <SignUp />
-  </div>
+  </SignInAndSignUpContainer>
 );
 
 export default SignInSignUpPage;

abbe000d9116459e5520d8eecfa76921a2ac2f7e
Jevvilla <juan.es-19@hotmail.com>
2020-04-30 18:21:35 -0500

    firebase utils file indented properly

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,12 +19,12 @@ const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) return;
+  if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapshot = await userRef.get();
 
-  if(!snapshot.exists) {
+  if (!snapshot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
@@ -33,15 +33,15 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (err) {
-      console.log('Error creating user: ', err.message);
+      console.log("Error creating user: ", err.message);
     }
   }
-  
+
   return userRef;
-}
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
